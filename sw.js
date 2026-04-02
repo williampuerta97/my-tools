528e74431e0409e2a288c544e9e2adb93c58fe06
William Puerta <williampuerta1097@gmail.com>
2026-04-01 22:53:55 -0500

    feat: adding config for pwa app, and item for total expenses

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'finanzas-v1';
+
+const urlsToCache = [
+  './',
+  './index.html',
+  './app_finanzas_personales_mensuales.html',
+  './simulador_credito_abonos.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// instalar
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// activar (limpiar cache viejo)
+self.addEventListener('activate', event => {
+  const whitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (!whitelist.includes(key)) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
